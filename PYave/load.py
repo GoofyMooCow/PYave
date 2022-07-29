e8985bb260b5249c8c325a54c123cbff043b1402
GoofyMooCow <64374601+GoofyMooCow@users.noreply.github.com>
2022-07-29 15:32:40 -0400

    Delete PYave directory

--- PYave/load.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-def load(title):
-    f = open(title + '.sv', 'r')
-    contents = f.read()
-    f.close()
-    return(contents)
--- PYave/save.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-def save(stuffToStore, title):
-    f = open(title + '.sv', 'w')
-    f.write(stuffToStore)
-    f.close()
