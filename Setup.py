f1a6c52c1cd54ba732d0981b731218ce38efd00c
QC1159 <64374601+QC1159@users.noreply.github.com>
2024-03-08 09:37:36 -0500

    Update Setup.py

--- Setup.py
+++ Setup.py
@@ -2,9 +2,8 @@ from setuptools import setup
 setup(name='PYave',
       version='0.5',
       description='Save and load data.',
-      url='https://github.com/HaneyFamily/PYave',
-      author='Jim',
-      author_email='PYave1@outlook.com',
+      url='https://github.com/QC1159/PYave',
+      author='QC1159',
       license='MIT',
       packages=['PYave'],
       zip_safe=False)
