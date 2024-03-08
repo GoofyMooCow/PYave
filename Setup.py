12579b438d3cd6008986ab11b83c8c3fb923e8c7
QC1159 <64374601+QC1159@users.noreply.github.com>
2024-03-08 09:56:53 -0500

    Change Name

--- Setup.py
+++ Setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup
-setup(name='PYave',
+setup(name='PythonSave',
       version='0.5',
       description='Save and load data.',
-      url='https://github.com/QC1159/PYave',
+      url='https://github.com/QC1159/PythonSave',
       author='QC1159',
       license='MIT',
-      packages=['PYave'],
+      packages=['PythonSave'],
       zip_safe=False)
